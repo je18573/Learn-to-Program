3ab84d3155e40e946b6edb89c6206a8febc80307
YuHua, Wu <je18573@gmail.com>
2014-03-03 17:59:02 +0800

    Add Chapter 2 all practice

--- ch2/calc.rb
+++ ch2/calc.rb
@@ -1,2 +1,43 @@
 puts 1+2
 puts 3
+
+#2.4 Simple Arithmetic
+
+# try it with floats
+puts '======== floats ================'
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# try it with integers
+puts '======== integers ================='
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+# try complex expressions
+puts '======== complex expressions ================='
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
+
+# Hours in a year. How many hours in a year?
+puts '======== How many hours in a year? ========'
+puts 365 * 24 
+puts 'hours.'
+
+# Minutes in a decade. How many minutes are in a decade?
+puts '======== How many minutes are in a decade? ========'
+puts 10 * 365 * 24 * 60
+puts 'minutes.'
+
+# Your age in seconds. How many seconds old are you?
+puts '======== How many seconds old are you? ======== '
+puts 25 * 365 * 24 * 60 * 60
+puts 'seconds.'
+
+# tougher question
+puts "Our dear author's age. If I am 1,160 million seconds old, How old am I?"
+puts 1160000000 / 60 / 60 / 24 / 365
+puts 800000000 / 60 / 60 / 24 / 365
