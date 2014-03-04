66fe40c4bb6f86636eba894edaf416d3420ee961
YuHua, Wu <je18573@gmail.com>
2014-03-04 10:08:58 +0800

    Add Ch3 Letters practice file

---
+++ ch3_Letters.rb
@@ -0,0 +1,33 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# String Arithmetic
+puts '================================='
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4
+
+# 12 vs. '12'
+puts '================================='
+puts 12 + 12		#24
+puts '12' + '12'#1212
+puts '12 + 12'	#12 + 12
+
+puts 2 * 5  	# 10
+puts '2' * 5	#22222
+puts '2 * 5'  #2 * 5
+
+# Problems
+puts '================================='
+#puts '12' + 12
+#puts '2' * '5'
+#puts 'Betty' + 12
+#puts 'Fred' * 'John'
+#puts 5 * 'pig'
+#puts 'You're swell'
+puts 'You\'re swell'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
