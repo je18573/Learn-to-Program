999b2d3767c59e4884831b054d01faf763aa32d8
YuHua, Wu <je18573@gmail.com>
2014-03-07 22:13:20 +0800

    practice to 7.2 branching finished.

--- ch7_Flow_Control.rb
+++ ch7_Flow_Control.rb
@@ -52,3 +52,18 @@ else
 	end
 end
 
+#
+puts 'Hello, and welcome to seventh grade English.'
+puts 'My name is Mrs. Gabbard. And your name is....?'
+name = gets.chomp
+
+if name == name.capitalize
+	puts 'Please take a seat, ' + name + '.'
+else
+	puts name + '? You mean ' + name.capitalize + ', right?'
+	reply = gets.chomp
+	if reply.downcase == 'yes'
+	else
+		
+	end
+end
