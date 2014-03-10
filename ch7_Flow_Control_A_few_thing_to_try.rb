76e96301fa2264eefeb5c5f08ba881df2891b3ec
YuHua, Wu <je18573@gmail.com>
2014-03-10 17:06:52 +0800

    finish ch7.4 #1 practice
    print the song lyric "99 bottles of beer on the wall"

--- ch7_Flow_Control_A_few_thing_to_try.rb
+++ ch7_Flow_Control_A_few_thing_to_try.rb
@@ -1,1 +1,21 @@
 #1 99 Bottles of Beer on the wall
+start_num = 99;
+now_num = start_num;
+
+while now_num > -1
+
+	if now_num == 0
+		first_string = 'No more bottles of beer on the wall, no more bottles of beer.'
+		second_string = 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+	else
+		first_string = now_num.to_s + ' bottles of beer on the wall, ' + now_num.to_s + ' bottles of beer.'
+		second_string = 'Take one down and pass it around, ' + (now_num - 1).to_s + ' bottles of beer on the wall.'
+	end
+
+	puts first_string
+	puts second_string
+	puts ''	
+	now_num = now_num - 1
+
+end
+
